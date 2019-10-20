bda8b01148d8f068203744cc16bcad1044fc8d2e
Michael Carroll <carrollm2@gmail.com>
2019-10-20 20:07:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -137,7 +137,7 @@ def adding_to_dennis
         }
      }
 
-     programmer_hash[:dennis_ritchie][:languages].push("Assmembly")
+     programmer_hash[:dennis_ritchie][:languages].push("Assembly")
 
      return programmer_hash
 
